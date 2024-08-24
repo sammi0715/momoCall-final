8c60656f440c41886228fd21c60346ba545123a5
jylee0805 <smexoshinee17@gmail.com>
2024-08-24 22:41:53 +0800

    style: the type in section set fixed

--- src/pages/ProductChat/Chat.jsx
+++ src/pages/ProductChat/Chat.jsx
@@ -4,7 +4,7 @@ import { FiAlertTriangle } from "react-icons/fi";
 
 function Chat() {
   return (
-    <div className="mt-[160px] bg-black-600 h-full py-4 px-3 flex flex-col gap-4">
+    <div className="mt-[160px] mb-12 bg-black-600 h-full py-4 px-3 flex flex-col gap-4">
       <p className="flex items-center bg-accent rounded-[20px] text-center text-sm leading-5 py-1 ">
         <FiAlertTriangle className="w-5 h-5 ml-6 mr-4" />
         提醒您，請勿透露個人資料及私下議價報價
--- src/pages/ProductChat/TypeIn.jsx
+++ src/pages/ProductChat/TypeIn.jsx
@@ -3,7 +3,7 @@ import { FiSend } from "react-icons/fi";
 
 function TypeIn() {
   return (
-    <div className="bg-primary-600 py-2 px-3 flex gap-2">
+    <div className="bg-primary-600 py-2 px-3 flex gap-2 fixed bottom-0 w-full">
       <button className="bg-white w-8 h-8 rounded-full active:border-primary active:border">
         <FiImage className="w-6 h-6 mx-auto text-primary hover:text-primary-800 " />
       </button>
