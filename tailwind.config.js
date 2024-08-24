42b38c9010aec9d56ae0458eee31c415e913c8d8
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-08-24 22:20:19 +0800

    chore: modify color configuration in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: {
+          600: '#D9D9D9',
+          100: '#020202',
+          80: '#525252',
+          60: '#919191',
+          40: '#D1D1D1',
+          20: '#F2F2F2',
+          0: '#FFFFFF',
+        },
+        primary: {
+          DEFAULT: '#E30681',
+          800: '#EA5A91',
+          600: '#FFAEBB',
+        },
+        secondary: {
+          DEFAULT: '#3876B5',
+          40: '#ABDAEA',
+        },
+        accent: '#FFF5D1',
+      },
+    },
+  },
+  variants: {
+
   },
   plugins: [],
 };
+
