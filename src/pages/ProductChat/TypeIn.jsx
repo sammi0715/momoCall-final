5c61243357f2f109da7fd5eceaeaa9985e4edcc5
jylee0805 <smexoshinee17@gmail.com>
2024-08-25 20:16:12 +0800

    style: add input outline style

--- src/pages/ProductChat/TypeIn.jsx
+++ src/pages/ProductChat/TypeIn.jsx
@@ -7,7 +7,7 @@ function TypeIn() {
       <button className="bg-white w-8 h-8 rounded-full active:border-primary active:border">
         <FiImage className="w-6 h-6 mx-auto text-primary hover:text-primary-800 " />
       </button>
-      <input type="text" placeholder="輸入訊息" className="bg-bg-white grow rounded-3xl pl-3 focus:outline-primary" />
+      <input type="text" placeholder="輸入訊息" className="bg-bg-white grow rounded-3xl pl-3  focus:outline-primary focus:outline" />
       <button className="bg-white w-8 h-8 rounded-full active:border-primary active:border">
         <FiSend className="w-5 h-5 mx-auto text-primary hover:text-primary-800" />
       </button>
