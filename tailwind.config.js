bf699fa4572a41608e12121fdd81aa78bfa6c02b
chunjull <chunjuchein@gmail.com>
2024-08-25 21:14:35 +0800

    feat: set custom variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,44 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "E30681",
+          800: "#EA5A91",
+          600: "#FFAEBB",
+        },
+        secondary: {
+          DEFAULT: "#3876B5",
+          400: "#ABDAEA",
+        },
+        accent: "#FFF5D1",
+        black: {
+          DEFAULT: "#020202",
+          800: "#525252",
+          600: "#919191",
+          400: "#D1D1D1",
+          200: "#F2F2F2",
+          0: "#FFFFFF",
+        },
+      },
+      width: {
+        container: "375px",
+        chatBox: "206px",
+        large: "70px",
+        middle: "60px",
+        small: "36px",
+        notice: "21px",
+      },
+      height: {
+        middle: "60px",
+        small: "36px",
+        notice: "21px",
+      },
+      rounded: {
+        large: "20px",
+      },
+    },
   },
   plugins: [],
 };
