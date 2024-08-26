968d67c96d347d61ac215741a958a5c1b73c39ac
jylee0805 <smexoshinee17@gmail.com>
2024-08-26 10:03:57 +0800

    style: merge files

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { StrictMode } from "react";
 import App from "./App.jsx";
-import ProductChat from "./pages/ProductChat/index.jsx";
+import ProductChat from "./pages/ProductChat";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
