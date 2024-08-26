8387a6d4685e467792221a270ef266f420cb4ebb
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-08-26 09:59:46 +0800

    style: fix container width

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -4,7 +4,7 @@ import momoLogo from "/momocallLogo.png";
 
 const SearchPages = () => {
   return (
-    <div className="w-[375px] max-w-screen min-h-screen m-[auto] bg-white justify-center p-3 pt-0 font-TC">
+    <div className="w-container max-w-screen min-h-screen m-[auto] bg-white justify-center p-3 pt-0 font-sans">
       <header className="flex items-center h-[68px]">
         <button className="flex-shrink-0">
           <FiChevronLeft size={24} />
@@ -25,12 +25,9 @@ const SearchPages = () => {
             key={item}
             className="w-[351px] h-[101px] flex items-center border-t border-gray-300"
           >
-            <img
-              className="w-[70px] h-[70px] rounded-full"
-              src={momoLogo}
-            ></img>
+            <img className="w-large h-large rounded-full" src={momoLogo}></img>
 
-            <div className="flex grow pl-4 flex-col justify-between">
+            <div className="flex grow pl-4 flex-col py-3 justify-between">
               <div className="flex justify-between">
                 <h2 className="text-base font-bold text-primary">
                   商家名稱最多也十二個字喔
