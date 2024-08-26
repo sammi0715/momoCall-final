ef313fe82764477a295d2c65d1c48ec287fad930
chunjull <chunjuchein@gmail.com>
2024-08-26 09:19:31 +0800

    frat: add font family

--- src/pages/Finish.jsx
+++ src/pages/Finish.jsx
@@ -6,7 +6,7 @@ import annoy from "../images/annoy.png";
 
 function Finish() {
   return (
-    <div className="bg-black-200 w-container h-100 my-0 mx-auto relative">
+    <div className="bg-black-200 w-container h-100 my-0 mx-auto relative font-sans">
       <div className="bg-black-200 w-container px-3 fixed top-0 left-0 right-0 z-10 my-0 mx-auto">
         <div className="flex items-center py-4">
           <FiChevronLeft className="w-6 h-6 mr-3 cursor-pointer" />
