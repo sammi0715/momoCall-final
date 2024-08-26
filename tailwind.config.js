00759d149f6ca634a051ffd7764e5395859033bf
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-08-26 09:13:39 +0800

    chore: update colors definition in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,25 +7,24 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        black: {
-          600: '#D9D9D9',
-          100: '#020202',
-          80: '#525252',
-          60: '#919191',
-          40: '#D1D1D1',
-          20: '#F2F2F2',
-          0: '#FFFFFF',
-        },
         primary: {
-          DEFAULT: '#E30681',
-          800: '#EA5A91',
-          600: '#FFAEBB',
+          DEFAULT: "#E30681",
+          800: "#EA5A91",
+          600: "#FFAEBB",
         },
         secondary: {
-          DEFAULT: '#3876B5',
-          40: '#ABDAEA',
+          DEFAULT: "#3876B5",
+          400: "#ABDAEA",
         },
-        accent: '#FFF5D1',
+        accent: "#FFF5D1",
+        black: {
+          DEFAULT: "#020202",
+          800: "#525252",
+          600: "#919191",
+          400: "#D1D1D1",
+          200: "#F2F2F2",
+          0: "#FFFFFF",
+        },
       },
       fontFamily: {
         TC: ["Noto Sans TC", "sans-serif"],
