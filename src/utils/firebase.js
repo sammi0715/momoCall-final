238d7058dfbffb9c8fd6fc6fe1a0f0b8468425bd
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-08-27 22:22:00 +0800

    feat: add getDocs, getDoc, and where functions

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,39 +1,47 @@
 import { initializeApp } from "firebase/app";
 import {
-  getFirestore,
-  collection,
-  addDoc,
-  setDoc,
-  doc,
-  query,
-  orderBy,
-  onSnapshot,
-  serverTimestamp,
+    getFirestore,
+    collection,
+    addDoc,
+    setDoc,
+    doc,
+    query,
+    orderBy,
+    onSnapshot,
+    serverTimestamp,
+    getDocs,
+    getDoc,
+    where,
+
+
 } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "momocall.firebaseapp.com",
-  databaseURL: "https://momocall-default-rtdb.firebaseio.com",
-  projectId: "momocall",
-  storageBucket: "momocall.appspot.com",
-  messagingSenderId: "179153261388",
-  appId: "1:179153261388:web:7936e0732579b355af35e9",
-  measurementId: "G-X76J3G62JR",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: "momocall.firebaseapp.com",
+    databaseURL: "https://momocall-default-rtdb.firebaseio.com",
+    projectId: "momocall",
+    storageBucket: "momocall.appspot.com",
+    messagingSenderId: "179153261388",
+    appId: "1:179153261388:web:7936e0732579b355af35e9",
+    measurementId: "G-X76J3G62JR",
 };
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 const db = getFirestore(app);
 export {
-  db,
-  storage,
-  collection,
-  addDoc,
-  doc,
-  setDoc,
-  query,
-  orderBy,
-  onSnapshot,
-  serverTimestamp,
+    db,
+    storage,
+    collection,
+    addDoc,
+    doc,
+    setDoc,
+    query,
+    orderBy,
+    onSnapshot,
+    serverTimestamp,
+    getDocs,
+    getDoc,
+    where,
 };
