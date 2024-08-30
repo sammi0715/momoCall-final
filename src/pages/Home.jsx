3ad4bd91b36bbcef8b2bfd8f1240ac19cff763dc
chunjull <chunjuchein@gmail.com>
2024-08-30 16:40:02 +0800

    feat: create layout for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import happy from "../images/happy.png";
+
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="w-container max-w-screen min-h-screen m-[auto] bg-black-200 flex flex-col justify-center items-center gap-4 px-3 font-sans">
+      <img src={happy} alt="" className="w-fit h-fit" />
+      <h2 className="text-xl leading-normal text-black font-bold">生活大小事，都是 momo 的事</h2>
+      <button className="bg-primary-800 text-black-0 rounded-large py-2 px-6">進入聊天室</button>
+      <button className="bg-secondary text-black-0 rounded-large py-2 px-6">管理者登入</button>
     </div>
   );
 }
