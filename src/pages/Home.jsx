d9fd16a78d72f835a2d27911b104c4932e7eb5e6
chunjull <chunjuchein@gmail.com>
2024-08-30 16:16:03 +0800

    feat: add layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 function Home() {
   return (
-    <div>
+    <div className="w-container max-w-screen h-screen m-[auto] bg-black-200 flex flex-col justify-center items-center gap-4 px-3">
       <h1>Home</h1>
+      <h2 className="text-xl leading-normal font-bold">生活大小事，都是 momo 的事</h2>
+      <button className="bg-primary-800 text-black-0 text-base leading-normal py-2 px-6 rounded-large hover:bg-primary">進入聊天室</button>
+      <button className="bg-secondary text-black-0 text-base leading-normal py-2 px-6 rounded-large">管理者登入</button>
     </div>
   );
 }
